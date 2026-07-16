e3c6311d94da50dcfc0c92fd7f56d47a0a8a7b98
Ash <115380969+ash3087@users.noreply.github.com>
2026-07-16 16:51:10 -0400

    fixed api bug

--- client/src/api/auth.js
+++ client/src/api/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = "http://localhost:3000/api/auth";
+const API = `${import.meta.env.VITE_API_URL}/auth`;
 
 export const registerUser = (data) => {
   return axios.post(`${API}/register`, data);
